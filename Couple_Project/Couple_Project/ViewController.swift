46d0685df1da87868b1ab1ff3d0b0cbac3c45138
eundeang <111877048+eundeang@users.noreply.github.com>
2024-01-10 19:36:56 +0900

    Init: 프로젝트 폴더 생성

---
+++ Couple_Project/Couple_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Couple_Project
+//
+//  Created by 허은정 on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Couple_Project/Couple_ProjectUITests/Couple_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Couple_ProjectUITestsLaunchTests.swift
+//  Couple_ProjectUITests
+//
+//  Created by 허은정 on 1/10/24.
+//
+
+import XCTest
+
+final class Couple_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
